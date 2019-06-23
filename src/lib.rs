7d36f5f9a8c5f43c1e6912c914ec35dccfc82873
Bart Massey <bart@cs.pdx.edu>
2019-06-23 15:40:44 -0700

    implemented code skeleton

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+// Copyright © 2019 Bart Massey
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+///! Functions to compute various statistics on a slice of
+///! floating-point numbers.
+
+/// Type of statistics function. If the statistic
+/// is ill-defined, `None` will be returned.
+pub type StatFn = fn(&[f64]) -> Option<f64>;
+
+/// Arithmetic mean of input values. The mean of an empty
+/// list is 0.0.
+///
+/// # Examples:
+///
+/// ```
+/// # use stats::*;
+/// assert_eq!(Some(0.0), mean(&[]));
+/// ```
+/// ```
+/// # use stats::*;
+/// assert_eq!(Some(0.0), mean(&[-1.0, 1.0]));
+/// ```
+pub fn mean(nums: &[f64]) -> Option<f64> {
+    unimplemented!("no mean yet")
+}
+
+/// Standard deviation of input values. The standard
+/// deviation of an empty list is undefined.
+///
+/// # Examples:
+///
+/// ```
+/// # use stats::*;
+/// assert_eq!(None, stddev(&[]));
+/// ```
+/// ```
+/// # use stats::*;
+/// assert_eq!(Some(0.0), stddev(&[1.0, 1.0]));
+/// ```
+pub fn stddev(nums: &[f64]) -> Option<f64> {
+    unimplemented!("no stddev yet")
+}
+
+/// Median value of input values, taking the value closer
+/// to the beginning to break ties. The median
+/// of an empty list is undefined.
+///
+/// # Examples:
+///
+/// ```
+/// # use stats::*;
+/// assert_eq!(None, median(&[]));
+/// ```
+/// ```
+/// # use stats::*;
+/// assert_eq!(Some(0.0), median(&[0.0, 0.5, -1.0, 1.0]));
+/// ```
+pub fn median(nums: &[f64]) -> Option<f64> {
+    unimplemented!("no median yet")
+}
+
+/// L2 norm (Euclidean norm) of input values. The L2
+/// norm of an empty list is 0.0.
+///
+/// # Examples:
+///
+/// ```
+/// # use stats::*;
+/// assert_eq!(Some(0.0), l2(&[]));
+/// ```
+/// ```
+/// # use stats::*;
+/// assert_eq!(Some(0.5), l2(&[-1.0, 1.0]));
+/// ```
+pub fn l2(nums: &[f64]) -> Option<f64> {
+    unimplemented!("no l2 yet")
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,56 @@
 // Please see the file LICENSE in the source
 // distribution of this software for license terms.
 
+///! Compute a statistic on numbers presented one-per-line on
+///! standard input.
+
+use std::process::exit;
+
+/// Report proper usage and exit.
+fn usage() -> ! {
+    eprintln!("stats: usage: stats [--mean|--stddev|--median|--l2]");
+    exit(1);
+}
+
+/// Do the computation.
 fn main() {
-    println!("Hello, world!");
+    // Process the argument.
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        usage();
+    }
+    let target = &args[1];
+    let argdescs: &[(&str, stats::StatFn)] = &[
+        ("--mean", stats::mean),
+        ("--stddev", stats::stddev),
+        ("--median", stats::median),
+        ("--l2", stats::l2),
+    ];
+    let stat = argdescs
+        .iter()
+        .find(|(a, _)| a == target)
+        .unwrap_or_else(|| usage())
+        .1;
+
+    // Read the input.
+    use std::io::BufRead;
+    let nums: Vec<f64> = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|s| {
+            let s = s.unwrap_or_else(|e| {
+                eprintln!("error reading input: {}", e);
+                exit(-1);
+            });
+            s.parse::<f64>().unwrap_or_else(|e| {
+                eprintln!("error parsing number {}: {}", s, e);
+                exit(-1);
+            })
+        })
+        .collect();
+
+    // Run the stat, show the result if any.
+    if let Some(result) = stat(&nums) {
+        println!("{}", result);
+    }
 }
